ecbba11ac807e62499b3f931ad357d9095d42d23
xanderlaabs <127143763+xanderlaabs@users.noreply.github.com>
2023-03-07 12:48:14 -0600

    Update starter.py

--- starter.py
+++ starter.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 
 url='https://austindowntownlions.org/Eyeglass_Recycling'
-pagestuff=requests/get(url).content
+pagestuff=requests.get(url).content
 placelist=pd.read_html(pagestuff)
 placelistdata=placelist
 print(placelistdata)
