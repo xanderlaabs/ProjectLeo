84353c00a0b08e55c7441dadecd253fa43247879
xanderlaabs <127143763+xanderlaabs@users.noreply.github.com>
2023-03-07 12:47:33 -0600

    Python script to get web data from lions club and convert to CSV format

---
+++ starter.py
@@ -0,0 +1,8 @@
+import requests
+import pandas as pd
+
+url='https://austindowntownlions.org/Eyeglass_Recycling'
+pagestuff=requests/get(url).content
+placelist=pd.read_html(pagestuff)
+placelistdata=placelist
+print(placelistdata)
