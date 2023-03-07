8e6d8f40d8e830c4d58778a910ab2b6efe4fb584
xanderlaabs <127143763+xanderlaabs@users.noreply.github.com>
2023-03-07 15:09:19 -0600

    So far cant find table on URL

--- starter.py
+++ starter.py
@@ -1,8 +1,10 @@
 import requests
 import pandas as pd
 
+
 url='https://austindowntownlions.org/Eyeglass_Recycling'
 pagestuff=requests.get(url).content
-placelist=pd.read_html(pagestuff)
+placelist=pd.read_html(pagestuff,match='.+')
 placelistdata=placelist
 print(placelistdata)
+placelist.to_csv('C:\testdata1.csv')
