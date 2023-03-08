5292cff859f9983248a467df0b4e3d131cc71b7b
xanderlaabs <127143763+xanderlaabs@users.noreply.github.com>
2023-03-08 10:44:04 -0600

    CSV Formatted
    Should not have result of csv file with good formatting for ArcGIS pro

--- ChartScraper1.py
+++ ChartScraper1.py
@@ -7,16 +7,12 @@ dfa = df[1]#(skiprows=1,header=0)
 print(dfa)
 firstcsv=r"J:\firstcsv.csv"
 dfa.to_csv(firstcsv)
-dfb = pd.read_csv(firstcsv,skiprows=1, header=0)
+dfb = pd.read_csv(firstcsv,skiprows=1, header=0,index_col='Facility') #removes row, sets header to new first row
+#Sets index column to facility, auto deletes numeric first column
 dfb=dfb.drop(dfb.columns[[0]],axis=1) #removes extra column
 print(dfb) #removing first row sucessful to this point
-
-
-
-csvtesta=r"J:\csvtesta.csv"
-dfb.to_csv(csvtesta) #for testing csv at this point, has 2 extra colums
-
-
+csvfinal=r"J:\csvfinal.csv"
+dfb.to_csv(csvfinal) #for testing csv at this point, has 2 extra colums
 
 #csvlocation=r"J:LionsLocations.csv"
 #df[1].to_csv(csvlocation) #Saves to CSV file, can write to locations py has permissions to
