a2d9e1a8f97cc6707bb03454e9f1187e7f4832fd
xanderlaabs <127143763+xanderlaabs@users.noreply.github.com>
2023-03-08 07:51:48 -0600

    Cleanup with comments

--- ChartScraper1.py
+++ ChartScraper1.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import os
+import arcpy
 
-df = pd.read_html('https://www.austindowntownlions.org/Eyeglasses_Recycling')
+df = pd.read_html('https://www.austindowntownlions.org/Eyeglasses_Recycling') #Correct Web Address
 
-print(df[1])
+print(df[1]) #Selects Index 1 of 0,1,2 , then prints out here to confirm
+df[1].to_csv('LionsLocations.csv') #Saves to CSV file
+
 
