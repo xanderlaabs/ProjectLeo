d77933eaf1bcab8c2f2410f4a5fe548125b18e56
xanderlaabs <127143763+xanderlaabs@users.noreply.github.com>
2023-03-07 18:03:55 -0600

    Cant find table
    Cant seem to find the right tbale on the website

--- starter.py
+++ starter.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 url='https://austindowntownlions.org/Eyeglass_Recycling'
 pagestuff=requests.get(url).content
-placelist=pd.read_html(pagestuff,match='.+')
-placelistdata=placelist
+placelist=pd.read_html(pagestuff,attrs={'class':'table'})
+#print(len(placelist))
+placelistdata=placelist[2]
 print(placelistdata)
-placelist.to_csv('C:\testdata1.csv')
+#placelist.to_csv('C:\testdata1.csv')
