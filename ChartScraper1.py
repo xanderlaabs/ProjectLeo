270a7f4228b0ed9ca5ef2f058baa1e277ce3e626
xanderlaabs <127143763+xanderlaabs@users.noreply.github.com>
2023-03-08 09:26:55 -0600

    Udes OutTableName to specify name, corrected gdb location to ProjectLeo Folder

--- ChartScraper1.py
+++ ChartScraper1.py
@@ -9,8 +9,9 @@ print(df[1]) #Selects Index 1 of 0,1,2 , then prints out here to confirm
 csvlocation=r"J:LionsLocations.csv"
 df[1].to_csv(csvlocation) #Saves to CSV file, can write to locations py has permissions to
 
-#gdblocation=r"E:\OneDriveDCCCD\ProjectLeo.gdb" #Setting up to output to GDB
-#arcpy.conversion.TableToTable(csvlocation,gdblocation,LionsTable) #Converting and adding to gdb
+gdblocation=r"E:\OneDriveDCCCD\ProjectLeo\ProjectLeo.gdb" #Setting up to output to GDB
+OutTableName="LionsTable"
+arcpy.conversion.TableToTable(csvlocation,gdblocation,OutTableName) #Converting and adding to gdb
 
 #arcpy.geocoding.GeocodeLocationsFromTable(LionsTable,)
 
