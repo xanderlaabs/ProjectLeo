227cb90a3a009a49d8ffd606e6d50b45f2dfe858
xanderlaabs <127143763+xanderlaabs@users.noreply.github.com>
2023-03-08 08:52:17 -0600

    Changed environemt
    Should now have access to ArcPy and maybe ArcGIS Pro stuff

--- ChartScraper1.py
+++ ChartScraper1.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import os
-import arcpy
+import arcpy   #for functions that have to run in ArcGIS Pro
 
 df = pd.read_html('https://www.austindowntownlions.org/Eyeglasses_Recycling') #Correct Web Address
 
 print(df[1]) #Selects Index 1 of 0,1,2 , then prints out here to confirm
-df[1].to_csv('LionsLocations.csv') #Saves to CSV file
 
+csvlocation=r"J:LionsLocations.csv"
+df[1].to_csv(csvlocation) #Saves to CSV file, can write to locations py has permissions to
+
+#gdblocation=r"E:\OneDriveDCCCD\ProjectLeo.gdb" #Setting up to output to GDB
+#arcpy.conversion.TableToTable(csvlocation,gdblocation,LionsTable) #Converting and adding to gdb
+
+#arcpy.geocoding.GeocodeLocationsFromTable(LionsTable,)
 
