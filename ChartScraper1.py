aa5707fe34d47ffa7a4d5720743f34b75b7e43c6
xanderlaabs <127143763+xanderlaabs@users.noreply.github.com>
2023-03-08 19:03:17 -0600

    Updated File Paths
    Moved ArcGIS working folder and scrath folder to different locations - should still work though

--- ChartScraper1.py
+++ ChartScraper1.py
@@ -5,16 +5,16 @@ import arcpy   #for functions that have to run in ArcGIS Pro
 df = pd.read_html('http://www.austindowntownlions.org/Eyeglasses_Recycling') #Correct Web Address
 dfa = df[1]#(skiprows=1,header=0)
 print(dfa)
-firstcsv=r"J:\firstcsv.csv"
+firstcsv=r"D:\ScratchFolder\firstcsv.csv"
 dfa.to_csv(firstcsv)
 dfb = pd.read_csv(firstcsv,skiprows=1, header=0,index_col='Facility') #removes row, sets header to new first row
 #Sets index column to facility, auto deletes numeric first column
 dfb=dfb.drop(dfb.columns[[0]],axis=1) #removes extra column
 print(dfb) #removing first row sucessful to this point
-csvfinal=r"J:\csvfinal.csv"
+csvfinal=r"D:\ScratchFolder\csvfinal.csv"
 dfb.to_csv(csvfinal) #for testing csv at this point, has 2 extra colums
 
-gdblocation=r"E:\OneDriveDCCCD\ProjectLeo\ProjectLeo.gdb" #Setting up to output to GDB
+gdblocation=r"E:\OneDriveMain\OneDrive\_GISProjects\2023Projects\ProjectLeo\ProjectLeo.gdb" #Setting up to output to GDB
 OutTableName="LionsTable"
 arcpy.conversion.TableToTable(csvfinal,gdblocation,OutTableName) #Converting and adding to gdb
 
