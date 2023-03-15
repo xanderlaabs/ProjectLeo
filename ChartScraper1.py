905e14583a9a84d5d8117643f79e0e5566a86f40
xanderlaabs <127143763+xanderlaabs@users.noreply.github.com>
2023-03-15 15:22:54 -0500

    Still adding to map
    Have been able to create a layer out of the feature class, but still cant seem to add it to the map at this point.

--- ChartScraper1.py
+++ ChartScraper1.py
@@ -3,34 +3,34 @@ import os
 import arcpy   #for functions that have to run in ArcGIS Pro
 
 gdbpath=r"E:\OneDriveMain\OneDrive\_GisProjects\2023Projects\ProjectLeo\Default.gdb"
-print("~~|  Geodatabase Path Set to:" + gdbpath)
+print("~~|   Geodatabase Path Set to:" + gdbpath)
 
 
 df = pd.read_html('http://www.austindowntownlions.org/Eyeglasses_Recycling')#Correct Web Address
 print("   ")
-print("~~| Reading Info From Website...")
+print("~~|  Reading Info From Website...")
 dfa = df[1]#(skiprows=1,header=0)
 print(dfa)
 firstcsv=r"D:\ScratchFolder\firstcsv.csv"
 dfa.to_csv(firstcsv)
 print("   ")
-print("~~| Saved as CSV File... ")
+print("~~|  Saved as CSV File... ")
 dfb = pd.read_csv(firstcsv,skiprows=1, header=0,index_col='Facility') #removes row, sets header to new first row
 #Sets index column to facility, auto deletes numeric first column
 print("   ")
-print("~~| Formatting Data... ")
+print("~~|  Formatting Data... ")
 dfb=dfb.drop(dfb.columns[[0]],axis=1) #removes extra column
 print(dfb) #removing first row sucessful to this point
 csvfinal=r"D:\ScratchFolder\csvfinal.csv"
 dfb.to_csv(csvfinal) #for testing csv at this point, has 2 extra colums
 print("   ")
-print("~~| Output to CSV LionsTable")
+print("~~|  Output to CSV LionsTable")
 
 #Setting up to output to GDB
 OutTableName="LionsTable"
 arcpy.conversion.TableToTable(csvfinal,gdbpath,OutTableName) #Converting and adding to gdb
 print("   ")
-print("~~| Converted to table in ArcGIS...")
+print("~~|  Converted to table in ArcGIS...")
 
 
 
@@ -51,18 +51,15 @@ print("~~|  Attempting to Geocode Table Locations...")
 print("   ")
 print("~~|  Attempting to create a feature layer on the map...")
 ResultPath1=r"E:\OneDriveMain\OneDrive\_GISProjects\2023Projects\ProjectLeo\Default.gdb\GeocodedDonationLocations"
-LayerLabel="Donation Locations"
-arcpy.management.MakeFeatureLayer(ResultPath1,LayerLabel)
-print("~~|  Donation Location Layer Added")
-print("   ")
+
+layername="Donation Locations"
+arcpy.management.MakeFeatureLayer(ResultPath1,layername)
+p=arcpy.mp.ArcGISProject("current")
+m=listMaps("Map")[0]
+m.addlayer(layername,"TOP")
 
 
-############Attempting to add LionsTable to Map
-######May not be needed, going to try to process table from filename and add to Gdb with filename/arcpy
 
-#aprx=arcpy.mp.ArcGISProject(r"E:\OneDriveMain\OneDrive\_GISProjects\2023Projects\ProjectLeo\ProjectLeo.aprx")
-#addTab=arcpy.mp.Table(r"E:\OneDRiveMain\OneDrive\_GISProjects\2023Projects\ProjectLeo\Default.gdb\LionsTable")
-#m=aprx.listMaps("Lions*")[0]
-#arcpy.RefreshTOC() #Setting up default map as project map and adding LionsTable to it
 
-##################################################
+
+
