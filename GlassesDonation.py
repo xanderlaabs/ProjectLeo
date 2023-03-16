a661396a47f2a67c990c5c695042ef48aefc07fc
xanderlaabs <127143763+xanderlaabs@users.noreply.github.com>
2023-03-16 10:44:02 -0500

    Attempting start of sort
    This code now finds the distances from the user location and adds them to the Near_dist field in donation locations. Hopefully, it will also delete the rows with a -1 value, which mean out of range

--- GlassesDonation.py
+++ GlassesDonation.py
@@ -24,46 +24,46 @@ with open(secondcsv,"w",newline="") as file_writer:     #writing user input to s
 
 
 #SpotList=[InputAddress,InputZip] #Creating List from strings
-print("")
+
 
 gdbpath=r"E:\OneDriveMain\OneDrive\_GisProjects\2023Projects\ProjectLeo\Default.gdb"
 print("~~|   Geodatabase Path Set to:" + gdbpath)
 
 
 df = pd.read_html('http://www.austindowntownlions.org/Eyeglasses_Recycling')#Scraping Correct Web Address
-print("   ")
+
 print("~~|  Reading Info From Website...")
 dfa = df[1]#(skiprows=1,header=0)
-print(dfa)
+#print(dfa)
 firstcsv=r"D:\ScratchFolder\firstcsv.csv"
 dfa.to_csv(firstcsv)
-print("   ")
+
 print("~~|  Saved as CSV File... ")
 dfb = pd.read_csv(firstcsv,skiprows=1, header=0,index_col='Facility') #removes row, sets header to new first row
 #Sets index column to facility, auto deletes numeric first column
-print("   ")
+
 print("~~|  Formatting Data... ")
 dfb=dfb.drop(dfb.columns[[0]],axis=1) #removes extra column
 print(dfb) #removing first row sucessful to this point
 csvfinal=r"D:\ScratchFolder\csvfinal.csv"
 dfb.to_csv(csvfinal) #for testing csv at this point, has 2 extra colums
-print("   ")
+
 print("~~|  Output to CSV LionsTable")
 
 #Setting up to output to GDB
 OutTableName="LionsTable"
 arcpy.conversion.TableToTable(csvfinal,gdbpath,OutTableName) #Converting and adding Lions Locations to gdb
-print("   ")
+
 print("~~|  Lions Donation Locations Converted to table in ArcGIS...")
-print("")
+
 
 spottablename="SpotTable"
 arcpy.conversion.TableToTable(secondcsv,gdbpath,spottablename)
 print("~~|   User Address Converted to table in ArcGIS...")
-print("   ")
+
 
 print("~~|  Attempting to Geocode Table Locations...")
-print("   ")
+
 arcpy.geocoding.GeocodeAddresses(
     in_table=r"E:\OneDriveMain\OneDrive\_GISProjects\2023Projects\ProjectLeo\Default.gdb\LionsTable",
     address_locator=r"E:\OneDriveMain\OneDrive\_GISProjects\2023Projects\ProjectLeo\AustinLocatorDelta.loc",
@@ -76,9 +76,9 @@ arcpy.geocoding.GeocodeAddresses(
     output_fields="LOCATION_ONLY"
 )
 ######Geocoding the Table and adding point layer to Default GDB
-print("   ")
+
 print("~~|  Geocoding Locations Completed...")
-print("   ")
+
 
 print("~~|    Geocoding User Location into Feature Class")
 arcpy.geocoding.GeocodeAddresses(
@@ -92,12 +92,12 @@ arcpy.geocoding.GeocodeAddresses(
     category=None,
     output_fields="LOCATION_ONLY"
 )#Creadted one entry layer with user input location, for use with Near tool
-print("   ")
+
 print("~~|    User Location Geocoded...")
 
 
 
-print("   ")
+
 print("~~|  Attempting to create a feature layer on the map...")
 ResultPath1=r"E:\OneDriveMain\OneDrive\_GISProjects\2023Projects\ProjectLeo\Default.gdb\GeocodedDonationLocations"
 ##Making Feature Layer from Feature Class, adding to map
@@ -106,10 +106,10 @@ layerfile1=arcpy.management.MakeFeatureLayer(ResultPath1,layername)[0]
 p=arcpy.mp.ArcGISProject(r"E:\OneDriveMain\OneDrive\_GISProjects\2023Projects\ProjectLeo\ProjectLeo.aprx")
 m=p.listMaps()[0]
 m.addLayer(layerfile1,"TOP")
-print("")
+
 print("~~|   Donation Locations Layer Added")
 
-print("")
+
 print("~~|   Adding User Location Layer")
 ResultPath1=r"E:\OneDriveMain\OneDrive\_GISProjects\2023Projects\ProjectLeo\Default.gdb\GeocodedUserLocation"
 layername="User Location"
@@ -118,6 +118,34 @@ m=p.listMaps()[0]
 m.addLayer(layerfile1,"TOP")
 
 print("~~|   User Location Layer Added")
+
+print("~~|   Calculating closest location to User Address...")
+arcpy.analysis.Near(
+    in_features="Donation Locations",
+    near_features="'User Location'",
+    search_radius=None,
+    location="NO_LOCATION",
+    angle="NO_ANGLE",
+    method="PLANAR",
+    field_names="NEAR_FID NEAR_FID;NEAR_DIST NEAR_DIST",
+    distance_unit="Miles"
+)
+
+lyr="Donation Locations"
+with arcpy.da.UpdateCursor(lyr,"NEAR_DIST") as cursor:
+    for row in cursor:
+        if row[0]==-1
+            cursor.deleteRow()
+
+print("~~|   Calculation complete")
+
+#arcpy.management.Sort(
+#    in_dataset="Donation Locations",
+#    out_dataset=r"E:\OneDriveMain\OneDrive\_GISProjects\2023Projects\ProjectLeo\Default.gdb\DonationLocationsSorted",
+#    sort_field="NEAR_DIST ASCENDING",
+#    spatial_sort_method="UR"
+#)
+
 
 #print("~~|   Changing Symbology of Donation Locations")
 #lyr=m.listLayers("Donation Locations")[0]
@@ -140,18 +168,4 @@ print("~~|   User Location Layer Added")
 #print("~~|   Symbology Changed")
 
 
-p.save()  #Will want to move this save around for before launching ArcGIS Pro
-
-#arcpy.analysis.GenerateNearTable(
-#    in_features="GeocodedUserLocation",
-#    near_features="GeocodedDonationLocations",
- #   out_table=r"E:\OneDriveMain\OneDrive\_GISProjects\2023Projects\ProjectLeo\Default.gdb\NearTable",
- #   search_radius=None,
- #   location="NO_LOCATION",
-#    angle="NO_ANGLE",
-#    closest="ALL",
-#    closest_count=5,
- #   method="PLANAR",
- #   distance_unit=""
-#)
-
+p.save()  
